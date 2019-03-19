d03069490ff6a453fdfb5a26f18979721ee90806
Olivier Decock <olivdeco1@arteveldehs.be>
2019-03-19 18:17:10 +0100

    v3.0.0

--- VibeFeed/App.js
+++ VibeFeed/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View, AsyncStorage } from 'react-native';
+import { Container, Content, Header, Form, Input, Item, Button, Label } from 'native-base'
 import * as firebase from 'firebase';
-
 
 
 // Initialize Firebase
@@ -17,19 +16,22 @@ const config = {
 firebase.initializeApp(config);
 
 
-import { Container, Content, Header, Form, Input, Item, Button, Label } from 'native-base'
-
 export default class App extends React.Component {
   
   constructor(props) {
     super(props)
 
     this.state = ({
-      email: '',
-      password: ''
+      email: "",
+      password: "",
+      newPassword: "",
+      currentPassword: "",
+      newEmail: "",
     })
   }
 
+
+  // Sign user up
   signUpUser = (email, password) => {
     
     try {
@@ -44,6 +46,9 @@ export default class App extends React.Component {
     }
   }
 
+
+
+  // Log user in
   loginUser = (email, password) => {
     
     try {
@@ -55,16 +60,76 @@ export default class App extends React.Component {
         console.log(error.toString())
     }
   }
+
+
+  // Add quote to firebase
+  addQuote = (quote, email) => {
+    firebase.database().ref('quotes/').push({
+      likes: 0,
+      quotes: quote,
+      userMail: email,
+    })
+  }
+
+  // Chance Password
+  reauthenticate = (currentPassword) => {
+    let user = firebase.auth().currentUser
+    let cred = firebase.auth.EmailAuthProvider.credential(user.email, currentPassword);
+    return user.reauthenticateWithCredential(cred);
+  }
+
+
+  onChangePasswordPress = () => {
+
+    this.reauthenticate(this.state.currentPassword).then(() => {
+
+      let user = firebase.auth().currentUser;
+      user.updatePassword(this.state.newPassword).then(() => {
+        alert.alert("Password was changed");
+      }).catch((error) => {
+        alert.alert(error.message);
+      }) 
+
+    }).catch((error) => {
+      alert.alert(error.message);
+    })
+   
+  }
+
+  // Chance email
+  onChangeEmailPress = () => {
+
+    this.reauthenticate(this.state.currentPassword).then(() => {
+
+      let user = firebase.auth().currentUser;
+      user.updateEmail(this.state.newEmail).then(() => {
+        alert.alert("Email was changed");
+      }).catch((error) => {
+        alert.alert(error.message);
+      }) 
+
+    }).catch((error) => {
+      alert.alert(error.message);
+    })
+    
+  }
+
+  // Logout
+  logout = () => {
+    firebase.auth().signOut();
+  }
   
   render() {
     return (
       <Container style={styles.container}>
+        
+        {/* LOGIN & Registreer */}
         <Form>
           <Item floatingLabel>
             <Label>Email</Label>
             <Input
               autoCorrect={false}
-              autoCapitalize="none"
+              au toCapitalize="none"
               onChangeText={(email) => this.setState({email})}
             />
           </Item>
@@ -79,22 +144,67 @@ export default class App extends React.Component {
             />
           </Item>
 
-          <Button
-            full
-            success
-            onPress={()=>this.loginUser(this.state.email,this.state.password)}
-          >
+          <Button full success onPress={()=>this.loginUser(this.state.email,this.state.password)}>
             <Text>Login</Text>
           </Button>
 
-          <Button
-            full
-            primary
-            onPress={()=>this.signUpUser(this.state.email,this.state.password)}
-          >
+          <Button full primary onPress={()=>this.signUpUser(this.state.email,this.state.password)}>
             <Text>Sign Up</Text>
           </Button>
         </Form>
+
+        
+        {/* ADD QUOTE TO FIREBASE */}
+        <Form>
+          <Item floatingLabel>
+            <Label>Add Quote</Label>
+            <Input
+              autoCorrect={false}
+              autoCapitalize="none"
+              onChangeText={(quote) => this.setState({quote})}
+            />
+          </Item>
+            
+          <Button full onPress={()=>this.addQuote(this.state.quote)}>
+            <Text>Add quote</Text>
+          </Button>
+        </Form>
+
+        {/* RESET PASSWORD */}
+        <Input value={this.state.newPassword}
+          placeholder="New Password"
+          autoCapitalize="none"
+          secureTextEntry={true}
+          onChangeText={(text) => {this.setState({newPassword: text})}}
+        />
+
+        <Input value={this.state.currentPassword}
+          placeholder="currentPassword"
+          autoCapitalize="none"
+          secureTextEntry={true}
+          onChangeText={(text) => {this.setState({currentPassword: text})}}
+        />
+
+        <Button onPress={this.onChangePasswordPress}>
+          <Text>Change Password</Text>
+        </Button>
+
+        {/* CHANCE EMAIL */}
+        <Input value={this.state.newEmail}
+          placeholder="newEmail"
+          autoCapitalize="none"
+          keyboardType="email-address"
+          onChangeText={(text) => {this.setState({newEmail: text})}}
+        />
+
+        <Button onPress={this.onChangeEmailPress}>
+          <Text>Change email</Text>
+        </Button>
+
+        {/* Logout */}
+        <Button onPress={this.logout()}>
+          <Text>Logout</Text>
+        </Button>
       </Container>
     );
   }
